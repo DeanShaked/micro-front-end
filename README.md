92a5c188367bb0863e084c6abfcb32b2c8e1bc61
Dean Shaked <deanshak9@gmail.com>
2022-03-28 23:21:58 +0300

    modified README.md

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # micro-front-end
-
+Components are shared in ```Singleton ``` mode, which means it shares ***only*** the pieces of code that are needed to use the component.
 ## Webpack configuration
-- home - exposing components for external usage.
-- pdp  - Using the exposed components by home.
-- cart - 
+```home ``` - exposing components for external usage.
+
+```pdp ``` - using the exposed components by home.
+
+```cart ```  
--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,17 +1,26 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import ReactDOM from "react-dom";
 
 // Async dynamic import
 const Header = React.lazy(() => import("home/Header"));
-const Footer = React.lazy(() => import("home/Footer"));
+// const Footer = React.lazy(() => import("home/Footer"));
+import Footer from "home/Footer";
 
 import "./index.scss";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Header />
-    <div className="my-10">PDP Page Content</div>
-    <Footer />
-  </div>
-);
+const App = () => {
+  const [showHeader, setShowHeader] = useState(true);
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      {showHeader && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header />
+        </Suspense>
+      )}
+      <button>Show The Header</button>
+      <div className="my-10">PDP Page Content</div>
+      <Footer />
+    </div>
+  );
+};
 ReactDOM.render(<App />, document.getElementById("app"));
