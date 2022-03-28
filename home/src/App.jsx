fb9d4ad836aa17b5d05bf01efdc40eb21c0bfa32
Dean Shaked <deanshak9@gmail.com>
2022-03-28 22:39:07 +0300

    added home and pdp, header and footer of home exposed to pdp

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import "./index.scss";
 
