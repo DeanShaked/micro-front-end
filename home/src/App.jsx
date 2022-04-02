bf8661e83c5168eade753eee629d100be258d32d
Dean Shaked <deanshak9@gmail.com>
2022-04-02 23:36:14 +0300

    added more details to home content

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeContent from "./components/HomeContent";
 
 import "./index.scss";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
     <Header />
-    <div className="my-10">Home Page Content</div>
+    <div className="my-10">
+      <HomeContent />
+    </div>
     <Footer />
   </div>
 );
